3a9d4bfac528267e684dc9eca134e29da73be579
KaMeRr <80587569+KaMeRr@users.noreply.github.com>
2022-01-23 20:48:33 +0100

    x

--- EPaczucha.database/EPaczuchaDbContext.cs
+++ EPaczucha.database/EPaczuchaDbContext.cs
@@ -5,9 +5,7 @@ namespace EPaczucha.database
 {
     public class EPaczuchaDbContext : IdentityDbContext
     {
-        public DbSet<Test> Tests { get; set; }
-        public DbSet<PackagePrice> PackagePrices { get; set; }
-        public DbSet<Pies> Piess { get; set; }
+        
         public EPaczuchaDbContext(DbContextOptions options) : base(options) { }
     }
 }
