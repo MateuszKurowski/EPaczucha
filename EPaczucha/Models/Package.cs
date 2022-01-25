c9cfe459261801e059a53ee4edb27f76d658dec2
KaMeRr <80587569+KaMeRr@users.noreply.github.com>
2022-01-25 01:43:09 +0100

    .

--- EPaczucha/Models/Package.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace EPaczucha.Models
-{
-    public class Package
-    {
-        public int Id { get; set; }
-        public string Name { get; set; }
-        public string Type { get; set; }
-    }
-}
